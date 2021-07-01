c2f2f95d984bfdfff2702220fdb401d7659923ad
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2021-07-01 23:04:31 +0200

    Add mscs from references

--- msch/zb_preview_record.py
+++ msch/zb_preview_record.py
@@ -4,7 +4,7 @@ from sickle.models import Record
 
 
 class ZbPreviewRecord(Record):
-    fieldnames = ['de', 'msc', 'keyword', 'title', 'text']
+    fieldnames = ['de', 'msc', 'keyword', 'title', 'text', 'refs']
 
     def get_de(self) -> int:
         return self.get_metadata()['document_id'][0]
@@ -24,11 +24,21 @@ class ZbPreviewRecord(Record):
     def get_text(self) -> str:
         return self.get_metadata()['review_text'][0]
 
+    def get_refs(self):
+        try:
+            refs = self.get_metadata()['ref_classification']
+            return refs
+        except KeyError:
+            return ""
+
     def writerow(self, writer: DictWriter):
         writer.writerow({
             'de': self.get_de(),
             'msc': self.get_msc(),
             'keyword': self.get_keywords(),
             'title': self.get_title(),
-            'text': self.get_text()
+            'text': self.get_text(),
+            'refs': self.get_refs(),
         })
+
+
