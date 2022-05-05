a797bf9fcf051a7c3c86d275f5492a66701ec843
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2022-05-05 21:10:29 +0200

    Filter zbl entries with band number

--- springer.py
+++ springer.py
@@ -1,11 +1,15 @@
 from main import run
+import re
 
-fieldnames = ["doi", "msc"]
+fieldnames = ["doi", "msc", "zbl_id"]
+full_id = re.compile(r'\d+\.\d')
 
 
-def filter(fields):
+def row_filter(fields):
     for k, v in fields.items():
         if v == "":
+            return False
+        if k == "zbl_id" and not full_id.match(v):
             return False
     return True
 
@@ -14,6 +18,6 @@ run(
     max_records=10000000,
     outfile="springer.csv",
     log_interval=1000,
-    row_filter=filter,
+    row_filter=row_filter,
     fieldnames=fieldnames,
 )
