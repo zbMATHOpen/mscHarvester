29eca60abc88ebe2f15e089d2daa1f6218105898
physikerwelt <wiki@physikerwelt.de>
2025-04-09 22:30:38 +0200

    Investigate structure of arxiv article
    Add test to check arxiv link structure

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -8,3 +8,8 @@ def test_get_client():
     assert len(row.get_refs()) == 45
     assert row.get_text() == ""
     assert row.get_doi() == "10.1515/math-2016-0103"
+
+def test_get_arxiv():
+    s = ZbPreviewSickle()
+    row = s.GetRecord(de=6377771)
+    assert row.get_attrib("link") == "https://arxiv.org/abs/1402.1748"
