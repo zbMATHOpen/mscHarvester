5bcdec7a9db08713fb65d6dcad8c7a9ff3a35283
physikerwelt <wiki@physikerwelt.de>
2025-04-09 22:58:43 +0200

    Arxiv attempt 1

--- arxiv.py
+++ arxiv.py
@@ -2,25 +2,28 @@ from main import run
 import re
 
 fieldnames = ["arXiv_id", "abstract"]
-full_id = re.compile(r'\d+\.\d')
+field_map =  {
+        "arXiv_id": "link",
+        "abstract": "review_text",
+    }
+# https://arxiv.org/abs/1402.1748
+arxiv = re.compile(r'https?://arxiv\.org')
 
 
 def row_filter(fields):
     for k, v in fields.items():
-        if v == "":
+        if v is None: # Remove articles without abstract
             return False
-        if k == "zbl_id" and not full_id.match(v):
+        if k=="arXiv_id" and not arxiv.search(v):
             return False
-    if isinstance(fields['msc'], list):
-        fields['msc'] = fields['msc'][0]
     return True
 
 
 run(
     max_records=10,
     outfile="arxiv.csv",
-    log_interval=1,
-    # row_filter=row_filter,
-    # fieldnames=fieldnames,
+    row_filter=row_filter,
+    fieldnames=fieldnames,
+    field_map=field_map,
     endpoint='https://oai.portal.mardi4nfdi.de/oai/OAIHandler'
 )
